e37dc176b7f66edd3f83792d1e8d853d50d5bda5
Dusan Klinec <dusan.klinec@gmail.com>
2016-07-20 16:05:42 +0200

    raw controller added

--- rest/src/main/java/com/enigmabridge/restgppro/GPController.java
+++ rest/src/main/java/com/enigmabridge/restgppro/GPController.java
@@ -3,8 +3,14 @@ package com.enigmabridge.restgppro;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pro.javacard.gp.GPArgumentTokenizer;
 import pro.javacard.gp.GPTool;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -17,10 +23,18 @@ public class GPController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        GPTool tool;
 
 
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
+    }
+
+    @RequestMapping("/raw")
+    public void rawRequest(@RequestParam(value = "request") String request, OutputStream output) throws IOException, NoSuchAlgorithmException {
+        final PrintStream ps = new PrintStream(output);
+        final GPTool tool = new GPTool(ps, ps);
+        final List<String> inputArgs = GPArgumentTokenizer.tokenize(request);
+        final int code = tool.work(inputArgs.toArray(new String[inputArgs.size()]));
+        ps.println("Done: " + code);
     }
 }
