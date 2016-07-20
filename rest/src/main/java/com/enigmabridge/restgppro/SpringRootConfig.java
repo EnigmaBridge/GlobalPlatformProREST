de12633e5aff844fb8c0a8fcbba6a755308426ae
Dusan Klinec <dusan.klinec@gmail.com>
2016-07-20 14:10:29 +0200

    rest works now

--- rest/src/main/java/com/enigmabridge/restgppro/SpringRootConfig.java
+++ rest/src/main/java/com/enigmabridge/restgppro/SpringRootConfig.java
@@ -1,7 +1,7 @@
 package com.enigmabridge.restgppro;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by dusanklinec on 20.07.16.
