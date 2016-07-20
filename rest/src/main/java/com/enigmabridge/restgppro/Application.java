9e828ae12b1d2dd5d45c81e0be6207c6207a60cf
Dusan Klinec <dusan.klinec@gmail.com>
2016-07-20 18:49:36 +0200

    logging fix, command line arguments - runner

--- rest/src/main/java/com/enigmabridge/restgppro/Application.java
+++ rest/src/main/java/com/enigmabridge/restgppro/Application.java
@@ -3,6 +3,9 @@ package com.enigmabridge.restgppro;
 /**
  * Created by dusanklinec on 20.07.16.
  */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
@@ -12,7 +15,9 @@ import org.springframework.context.annotation.Bean;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -23,5 +28,10 @@ public class Application {
         factory.setPort(8081);
         factory.setSessionTimeout(50, TimeUnit.MINUTES);
         return factory;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("Started...");
     }
 }
