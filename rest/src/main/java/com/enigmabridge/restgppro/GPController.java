812e13e9a57740e7008f14261f09594a0daa5cfd
Dusan Klinec <dusan.klinec@gmail.com>
2016-07-21 10:18:07 +0200

    code clean

--- rest/src/main/java/com/enigmabridge/restgppro/GPController.java
+++ rest/src/main/java/com/enigmabridge/restgppro/GPController.java
@@ -23,8 +23,6 @@ public class GPController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-
-
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
